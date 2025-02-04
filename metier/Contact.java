a4896a6283f1f30610daaf08c363055174dc2517
helder-hanka <hfernandes238@gmail.com>
2025-02-04 09:16:17 +0100

    Ajouter les methodes getters, setters et toString

--- metier/Contact.java
+++ metier/Contact.java
@@ -11,4 +11,27 @@ public class Contact {
     this.telephone = telephone;
    }
 
+   public String getNom(){
+    return this.nom;
+   }
+   public String getEmail(){
+    return this.email;
+   }
+   public String getTelephone(){
+    return this.telephone;
+   }
+
+   public void SetNom(String nom) {
+    this.nom = nom;
+   }
+   public void setEmail(String email) {
+    this.email = email;
+   }
+   public void setTelephone(String telephone) {
+    this.telephone = telephone;
+   }
+
+   public String ToString(){
+    return "Nom: "+this.nom+" Email: "+this.email+" Telephone: "+this.telephone;
+   }
 }
