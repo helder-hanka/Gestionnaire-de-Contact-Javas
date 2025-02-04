88ae6abb0f0a3b267a89da8cd2616e12c02dcf81
helder-hanka <hfernandes238@gmail.com>
2025-02-04 09:05:45 +0100

    Création class Contact, attribut et constructeur

---
+++ metier/Contact.java
@@ -0,0 +1,14 @@
+package metier;
+
+public class Contact {
+   private String nom;
+   private String email;
+   private String telephone;
+
+   public Contact(String nom, String email, String telephone){
+    this.nom = nom;
+    this.email = email;
+    this.telephone = telephone;
+   }
+
+}
