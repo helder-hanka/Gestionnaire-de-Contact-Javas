69f26ab28de4ba6a2d4346bbe7fcbc83b5315c33
helder-hanka <hfernandes238@gmail.com>
2025-02-04 08:36:15 +0100

    Création fiechiers pour le Projet

---
+++ presentation/Principale.java
@@ -0,0 +1,7 @@
+package presentation;
+
+public class Principale {
+    public static void main(String[] args) {
+        System.out.println("Bienvenue dans le Gestionnaire de Contacts!");
+    }
+}
